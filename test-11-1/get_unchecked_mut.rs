7c54bb4dbf558abbb64a6f6fd79a92fd0ee0fc68
yaokunzhang <2418358039@qq.com>
2024-10-31 21:26:12 +0800

    update

--- test-11-1/get_unchecked_mut.rs
+++ test-11-1/get_unchecked_mut.rs
@@ -3,7 +3,7 @@ fn main() {
 
     unsafe {
         let out_of_bounds_index = 5; // 越界索引
-        let out_of_bounds_index = 4; // 越界索引
+        // let out_of_bounds_index = 4; // 越界索引
         let out_of_bounds_ptr = array.get_unchecked_mut(out_of_bounds_index); // 取消注释将导致未定义行为
         *out_of_bounds_ptr = 30;
     }
